4035776524aa0f8e85296c78648f969f521b6704
kosi <hugo-pioda_student2020@wilder.school>
2024-12-17 22:04:00 +0100

    add fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './containers/App.jsx'
-import './scss/style.scss'
+import './scss/main.scss'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
