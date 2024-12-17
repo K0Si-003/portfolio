fb61020889ac85a7382c9b00119b95625f21e025
kosi <hugo-pioda_student2020@wilder.school>
2024-12-17 22:24:13 +0100

    add curtain

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import Particles from '../components/Particles.jsx'
+
 export default function Home() {
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    // Fake loading for curtain
+    setTimeout(() => {
+        setIsLoaded(true)
+    }, 300)
+
     return (
         <main>
             <h1>Home</h1>
             <div className="background">
                 <Particles />
             </div>
+            <div className={`curtain ${isLoaded ? 'hidden' : ''}`}></div>
         </main>
     )
 }
