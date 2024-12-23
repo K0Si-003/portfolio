d541f68ff9a0a371df7f68cc377502b272fc8ef1
kosi <hugo-pioda_student2020@wilder.school>
2024-12-23 19:24:59 +0100

    about page wip

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,35 @@
 export default function About() {
     return (
-        <main>
-            <h1>About</h1>
+        <main className="about">
+            <div className="container">
+                <h1 className="about__title">About</h1>
+                <div className="container-md">
+                    <p className="about__text">
+                        <span>
+                            Salut, moi c'est Hugo, développeur front-end basé à
+                            Lyon, France. Bienvenue sur mon petit bout
+                            d'internet. Ici vous pourrez trouver mon travail,
+                            mes derniers projets ou mes dernières
+                            expérimentations.
+                        </span>
+                    </p>
+                    <p className="about__text">
+                        <span>
+                            Mes premiers pas dans le monde du web se font en 2016 via la découverte de Flash et ActionScript.
+                            Par la suite je découvrirais, le webdesign, et le développement front-end.
+                            Html, CSS puis React et ThreeJS.
+                        </span>
+                    </p>
+                    <p className="about__text">
+                        <span>
+                            Comme prochaine étape d'apprentisage je souhaiterais approfondir les frameworks  orienté SSR.
+                            100% Typescript en ligne de mire et toujours des expérimentations dans le domaine de la 3d orienté web.
+                        </span>
+                    </p>
+                </div>
+                <h2>Skills</h2>
+            </div>
+            <div className="background"></div>
         </main>
     )
 }
