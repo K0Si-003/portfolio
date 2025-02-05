42aceae475e191d5630573bcd4d18c3a99e5a3a4
K0Si-003 <hugo.pioda@gmail.com>
2025-02-05 19:40:08 +0100

    rework about - spotify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from 'react'
 import { getSavedTracks } from '../utils/SpotifyAPI.js'
 import tabsContent from '../data/tabsContent.jsx'
 import Tabs from '../components/Tabs.jsx'
+import useMatchMedia from '../utils/useMatchMedia.jsx'
 
 export default function About() {
     const [fetchingData, setFetchingStatus] = useState(true)
     const [savedTracks, setSavedTracks] = useState({})
+    const [selectedTrackId, setSelectedTrackId] = useState(null)
+
+    const isMobile = useMatchMedia('(max-width: 992px)')
 
     const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID
     const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
@@ -17,6 +21,12 @@ export default function About() {
             setFetchingStatus(false)
         })
     }, [])
+
+    const handleClick = (trackId) => {
+        if (isMobile) {
+            setSelectedTrackId(trackId === selectedTrackId ? null : trackId)
+        }
+    }
 
     return (
         <main className="about">
@@ -43,17 +53,17 @@ export default function About() {
                     <span>
                         C'est par la suite, avec une formation à la Wild Code School, que je plonge
                         dans le grand bain du développement web, avec l'apprentisage de tous les
-                        outils du développement web moderne. J'y ai acquis des bases techniques
-                        (lien section skills) et méthodologiques.
+                        outils du développement web moderne. J'y ai acquis des bases{' '}
+                        <a href="#skills">techniques</a> et méthodologiques.
                     </span>
                 </p>
                 <p className="about__text">
                     <span>
-                        C'est ensuite par mes expériences personnelles et professionelles,
-                        que j'ai pu progresser sur le terrain. J'ai pu y apprendre l'utilisation de
-                        nouveau outils, les bonnes pratiques pour le réferencement. Je me suis aussi
-                        formé de mon côté sur de la 3D orienté web. L'utilisation de Three.js, React
-                        Three Fiber et Blender.
+                        C'est ensuite par mes expériences personnelles et professionelles, que j'ai
+                        pu progresser sur le terrain. J'ai pu y apprendre l'utilisation de nouveau
+                        outils, les bonnes pratiques pour le réferencement. Je me suis aussi formé
+                        de mon côté sur de la 3D orienté web. L'utilisation de Three.js, React Three
+                        Fiber et Blender.
                     </span>
                 </p>
                 <p className="about__text">
@@ -65,31 +75,37 @@ export default function About() {
                     </span>
                 </p>
             </section>
-            <section className="about__skills container-full">
-                <div className="skills__text container">
-                    <h2>Compétences</h2>
-                    <p>
-                        <span className="about__text">
+            <section className="about__skills container-full" id="skills">
+                <div className="skills__text container-md">
+                    <h2 className="about__title">Compétences</h2>
+                    <p className="about__text">
+                        <span>
                             Voici un aperçu des outils et langages que j'utilise pour mes projets.
                         </span>
                     </p>
                 </div>
-                <div className="skills__tabs container">
+                <div className="skills__tabs container-md">
                     <Tabs tabs={tabsContent} />
                 </div>
             </section>
             <section className="about__bonus container-md">
-                <h2>Bonus</h2>
-                <div className="more__text">
-                    <p>Grimpe, jeux de société, montagne, poele à bois et sport de glisse !</p>
-                </div>
-                <p>Un petit coin pour découvrir les dernières pépites dans mes oreilles</p>
+                <h2 className="about__title">Bonus</h2>
+                <p className="about__text">
+                    Grimpe, jeux de société, montagne, poele à bois et sport de glisse !
+                </p>
+                <p className="about__text">
+                    Un petit coin pour découvrir les dernières pépites dans mes oreilles
+                </p>
                 {fetchingData ? (
-                    <p>Loading...</p>
+                    <p>On dirais que Spotify ne veut pas nous montrer ma playlist...</p>
                 ) : (
                     <ul className="spotify__grid">
                         {savedTracks.items.map((track) => (
-                            <li key={track.track.id} className="grid__item">
+                            <li
+                                key={track.track.id}
+                                className={`grid__item ${selectedTrackId === track.track.id ? 'selected' : ''}`}
+                                onClick={() => handleClick(track.track.id)}
+                            >
                                 <p className="item__text">
                                     <span>{track.track.artists[0].name}</span>
                                     <span>{track.track.name}</span>
