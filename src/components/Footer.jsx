cd2e39f6846c6f77b001dd124b2b985a9a18ea35
K0Si-003 <hugo.pioda@gmail.com>
2025-02-06 22:14:38 +0100

    footer desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,72 @@
+import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router'
+import Socials from './Socials.jsx'
+import IconSvg from './IconSvg.jsx'
+import Arrow from '../assets/images/arrow-up.svg?react'
+import { siReact } from 'simple-icons'
+
 export default function footer() {
+    const [isVisible, setIsVisible] = useState(false)
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+
+    const toggleVisibility = () => {
+        if (window.scrollY > 300) {
+            setIsVisible(true)
+        } else {
+            setIsVisible(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility)
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility)
+        }
+    }, [])
+
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className="footer container-full">
+            <div className="footer__wrapper container">
+                <div className="footer__top">
+                    <div className="footer__social">
+                        <Socials />
+                    </div>
+                    <ul className="footer__nav">
+                        <li>
+                            <NavLink to="/">Accueil</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Projets</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">A propos</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="footer__bottom">
+                    <div className="bottom__left">
+                        <span>© 2025</span>
+                    </div>
+                    <div className="bottom__right">
+                        <span>
+                            {`</>`} with ❤️ and{' '}
+                            <IconSvg icon={siReact} color={'#61DAFB'} size={'16'} />
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <button className={`back-to-top${isVisible ? ' visible' : ''}`} onClick={scrollToTop}>
+                <Arrow />
+            </button>
         </footer>
     )
 }
--- src/components/IconSvg.jsx
+++ src/components/IconSvg.jsx
@@ -1,11 +1,11 @@
-export default function IconSvg({ icon, color }) {
+export default function IconSvg({ icon, color, size = '24' }) {
     return (
         <svg
             role="img"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
+            width={size}
+            height={size}
         >
             <path d={icon.path} fill={color} />
         </svg>
