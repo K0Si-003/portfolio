6069c6c535a4164071448d55e74aa4ad2454c4ce
K0Si-003 <hugo.pioda@gmail.com>
2025-03-01 22:26:46 +0100

    contact page for 3d map

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,11 @@
-import { Canvas } from '@react-three/fiber'
 import Map from '../components/Map.jsx'
-import Lights from '../components/Lights.jsx'
 
 export default function Contact() {
     return (
-        <main className='contact'>
-            <h2 className='contact__title'>Contact</h2>
-            <div className="contact__model">
-                <Canvas
-                    shadows
-                    camera={{
-                        fov: 75,
-                        near: 0.5,
-                        far: 10000,
-                        position: [-600, 20, 1000],
-                    }}
-                >
-                    <Lights />
-                    <Map />
-                </Canvas>
+        <main className="contact">
+            <h2 className="contact__title">Contact</h2>
+            <div className="contact__3d">
+                <Map />
             </div>
             <div className="background"></div>
         </main>
