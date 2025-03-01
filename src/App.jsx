b9b3a79810695709cbea4260b67a077e7927a5a2
K0Si-003 <hugo.pioda@gmail.com>
2025-03-01 22:24:12 +0100

    move file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from '../components/Header.jsx'
-import Router from '../routes/Router.jsx'
-import Footer from '../components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Router from './routes/Router.jsx'
+import Footer from './components/Footer.jsx'
 
 export default function App() {
     return (
